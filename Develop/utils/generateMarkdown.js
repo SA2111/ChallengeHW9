f5ddb3a3361a302a1a86960606e97254d1a0f600
SA2111 <127169775+SA2111@users.noreply.github.com>
2023-07-24 13:46:56 -0400

    Update generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -20,7 +20,25 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  
+${renderLicenseBadge(data.license)}
+// description link 
+## Description
+${data.description}
+// web link
+## Deployed Application
+${data.URL}
+// contents
+## Table of Contents
+* [Features](#coolItens)
+* [usage](#usage)
+* [Contributors](#contributors)
+## Features
+${data.coolItens}
+## usage
+${data.require}
+## Contributors
+${data.contributors}
 `;
 }
 
